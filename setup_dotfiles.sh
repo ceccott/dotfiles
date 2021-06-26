86955b94ee9119586287b82a48afc42219140319
ceccott <ceccott@gmail.com>
2021-06-26 16:59:46 +0200

    fixed tmux spotify config

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -6,7 +6,8 @@ wdir=${PWD}
 ln -sf ${wdir}/.vimrc 		${HOME}/
 ln -sf ${wdir}/.tmux.conf 	${HOME}/
 ln -sf ${wdir}/.zshrc 		${HOME}/
-ln -sf ${wdir}/nvim 		${HOME}/.config/ 
+ln -sf ${wdir}/nvim/init.vim 	${HOME}/.config/nvim/
+ln -sf ${wdir}/nvim/custom_snippets 	${HOME}/.config/nvim/
 
 echo "dotfiles linked"
 
